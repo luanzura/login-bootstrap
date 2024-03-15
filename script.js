036b9e4aed4e41da40a6841ed5978e8014166f4f
Luanzura <luanmaximus@gmail.com>
2024-03-15 10:25:43 -0300

    finalizado

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ document.getElementById('color-mode-toggle').addEventListener('click', function(
         this.textContent = 'light_mode';
     }
 });
+
+function signUp(){
+    document.getElementById("sign-up").style.display = "flex";
+    document.getElementById("sign-in").style.display = "none";
+}
+
+function signIn(){
+    document.getElementById("sign-up").style.display = "none";
+    document.getElementById("sign-in").style.display = "flex";
+}
+
+function register(){
+    event.preventDefault();
+
+    let password = document.getElementById("password").value;
+    let confirm_password = document.getElementById("confirm_password").value;
+
+    if (password !== confirm_password) {
+        document.getElementById("password-confirm-warning").style.display = "inline-block";
+        setTimeout(function() {
+            document.getElementById("password-confirm-warning").style.display = "none";
+        }, 5000);
+    } else {
+        console.log("Password OK!");
+    }
+}
